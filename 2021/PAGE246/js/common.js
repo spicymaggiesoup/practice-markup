229bde82eff0f494d8952c8fd7d904088fcb81e6
samantha93 <samantha8009@gmail.com>
2021-09-01 12:59:10 +0900

    doing page246

--- 2021/PAGE246/js/common.js
+++ 2021/PAGE246/js/common.js
@@ -5,4 +5,20 @@ $(document).ready(function() {
     }).on('mouseleave', function() {
         $(this).find('.submenu').removeClass('show');
     });
+
+    // 슬라이드 
+    var now = 0; var ttl = 2;
+    function fnSliderAni() {
+        $('.slider a').eq(now).fadeOut();
+        $('.slider a').eq(now + 1).siblings().fadeIn();
+    }
+    fnSliderAni();
+    setInterval(function() {
+        now = now > ttl ? 0 : now;
+
+        $('.slider a').eq(now + 1).fadeIn();
+        $('.slider a').eq(now).fadeOut();
+
+        now += 1;
+    }, 2800);
 });
