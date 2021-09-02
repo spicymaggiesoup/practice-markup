d12e857cc7fa6ca88a3b3e1ef50436cb2f14eee8
spicymaggiesoup <samantha8009@gmail.com>
2021-09-02 22:15:46 +0900

    new template completed

--- 2021/PAGE143/js/common.js
+++ 2021/PAGE143/js/common.js
@@ -1,6 +1,26 @@
 $(document).ready(function() {
       // 사이드메뉴
-      $('.sidemenu-title').on('click', function() {            
-            $(this).find('ul').attr('style', 'display:block');
+      $('.sidemenu-title').on('mouseenter', function() {            
+            $(this).find('ul').slideDown(250);
+      }).on('mouseleave', function() {            
+            $(this).find('ul').slideUp(250);
       });
+
+      // 슬라이드
+      var now = 0;
+      var ttl = 2;
+      $('.slider a').eq(0).fadeIn();
+      setInterval(function() {
+            now = now === ttl ? 0 : now;
+            $('.slider a').eq(now += 1).fadeIn();
+            $('.slider a').eq(now).fadeOut();
+      }, 1500);
 });
+
+function fnShowHideModal(param) {
+      if (param === 'show') {
+            $('.background').removeClass('hide');
+      } else {
+            $('.background').addClass('hide');
+      }
+}
