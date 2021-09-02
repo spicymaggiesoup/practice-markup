18c92436dac52d5565d749b3dede4ea3cf196b42
samantha93 <samantha8009@gmail.com>
2021-09-02 13:29:02 +0900

    add new pj

---
+++ 2021/PAGE143/js/common.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+      // 사이드메뉴
+      $('.sidemenu-title').on('click', function() {            
+            $(this).find('ul').attr('style', 'display:block');
+      });
+});
